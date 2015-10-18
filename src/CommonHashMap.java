8fe8b8787df8168d34429543ed7ecf1cf491d7c9
odisharia <teona.odisharia02@geolab.edu.ge>
2015-10-18 16:50:10 +0400

    no message

--- src/CommonHashMap.java
+++ src/CommonHashMap.java
@@ -6,18 +6,25 @@ import java.util.Scanner;
  * Created by Owl on 18.10.15.
  */
 public class CommonHashMap {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Map<String, String> map1 = new HashMap<String, String>();
         Map<String, String> map2 = new HashMap<String, String>();
-        String st;
-        System.out.println("Sheiyvanet masivis mniSvnelobebi, \n SHEYVANIS DASASRULEBLAD SHEIYVANET 0 !");
+        map1.put("Geolab", "Dzerski");
+        map1.put("Seriali", "Silicon Valley");
+        map1.put("Zamtari", "Civi");
+        map1.put("Irakli", "Mdzinara");
+        map1.put("Aba", "Ho");
 
-        Scanner sc = new Scanner();
-        st = sc.nextLine();
-        if (st != null) {
-            
-        }
+        map2.put("Irakli", "Mdzinara");
+        map2.put("Geolab", "Dzerski");
+        map2.put("Harry", "Potter");
+        map2.put("Tbilisoba", "mwvadi");
+        map2.put("Aba", "He");
+
+        String st;
 
 
     }
+    public int commonKeyValuePairs(HashMap<String, String> map1, HashMap<String, String> map2){}
+
 }
